d755c50537dda3c3df88b214407aea41b2332046
Anto Christopher <93177734+anto-deepsource@users.noreply.github.com>
2024-10-25 15:45:59 +0530

    Update ArrayUsage.cs

--- src/core-sdk-tasks/ArrayUsage.cs
+++ src/core-sdk-tasks/ArrayUsage.cs
@@ -6,9 +6,7 @@ public class ArrayUsage
   {
     ArrayDefinition arrayDef = new ArrayDefinition();
     var array = arrayDef.Numbers;
-    var array2 = new int[] { 1, 2, 3, 4, 5 };
-    // This will raise the issue as it calls .ToString() on an array
     Console.WriteLine(array.ToString());
-    Console.WriteLine(array2.ToString());
+    Console.WriteLine(arrayDef.Numbers.ToString());
   }
 }
